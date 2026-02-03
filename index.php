b0b3858b333f08fa6d9db4aba26b1eb637ffcfe5
Marlise098 <marlise.koemans@outlook.com>
2026-02-03 13:56:13 +0100

    heyyyyyyy

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ include ('connection.php');
         <h1>Welcome to SecretValentine</h1>
     </div>
 
+     <div id ="menu">
       <nav>
     <ul>
       <li>Valentine</li>
@@ -23,15 +24,22 @@ include ('connection.php');
       <li>Very pro</li>
     </ul>
   </nav>
-
+        </div>
+        
     <div id="content">
         <h2>Find Your Secret Valentine</h2>
         <p>Join our community and discover your secret valentine today!</p>
-
+     </div>
         <textarea name="Text" id="TextId"></textarea>
 
         <footer>
             <p>Happy Valentine's Day!</p>
-        </footer>
+        </footer
+
+     <div id = "button">
+        <button type="button">Send</button>
+     </div>
+      
+
 </body>
 </html>
