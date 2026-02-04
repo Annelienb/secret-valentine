349ffb6da4cbe91d4f85b0482fcd42a37ca8cc3c
Marlise098 <marlise.koemans@outlook.com>
2026-02-04 12:22:03 +0100

    mama mia

--- index.php
+++ index.php
@@ -10,36 +10,29 @@ include ('connection.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SecretValentine</title>
     <link rel="stylesheet" href="style.css">
+    <link  rel="icon" href="envelopee.png">
 </head>
 <body>
+
     <div id="header">
         <h1>Welcome to SecretValentine</h1>
     </div>
 
-     <div id ="menu">
-      <nav>
-    <ul>
-      <li>Valentine</li>
-      <li>Roses</li>
-      <li>Very pro</li>
-    </ul>
-  </nav>
-        </div>
-
     <div id="content">
         <h2>Find Your Secret Valentine</h2>
         <p>Join our community and discover your secret valentine today!</p>
-     </div>
+
         <textarea name="Text" id="TextId"></textarea>
 
-        <footer>
-            <p>Happy Valentine's Day!</p>
-        </footer
+        <div id="button">
+            <button type="button">Send</button>
+        </div>
+    </div>
 
-     <div id = "button">
-        <button type="button">Send</button>
-     </div>
-      
+    <div id="footer">
+        <p>Happy Valentine's Day!</p>
+    </div>
 
 </body>
+
 </html>
