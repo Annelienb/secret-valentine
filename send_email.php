4dd25875f38275be07106b09ecf3455c8224a3f5
Annelien <itsannelien@gmail.com>
2026-02-05 13:43:26 +0100

    update

--- send_email.php
+++ send_email.php
@@ -14,16 +14,3 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Message Sent Successfully!</h1>
-</body>
-</html>
->>>>>>> Stashed changes
