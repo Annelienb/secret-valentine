007bdc5debf8b7cb836646d8218bb18174aa7e9c
Annelien <itsannelien@gmail.com>
2026-02-05 13:41:21 +0100

    .

---
+++ send_email.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $to = $_POST["email"];
+    $message = $_POST["message"];
+    $subject = "💌 A Secret Valentine Message";
+    $headers = "From: secretvalentine@yourwebsite.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Message sent successfully 💖";
+    } else {
+        echo "Oops… something went wrong 😢";
+    }
+}
+?>
